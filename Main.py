250007338b1f6228ca692d1763003963974c91ef
yuvraj420190 <108419015+yuvraj420190@users.noreply.github.com>
2022-10-12 09:28:15 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,16 +1,23 @@
 import re
+
 def display_hash(hashtable) -> None:
-for keyvalue in range(len(hashtable)):
-cont = hashtable[keyvalue]
-print(keyvalue, end = " ")
-for element in cont:
-print("-->", element, end = " ")
-print()
+	for keyvalue in range(len(hashtable)): 
+		cont = hashtable[keyvalue]
+		print(keyvalue, end = " ")
+		for element in cont:
+
+			print("-->", element, end = " ")
+
+		print()
+
+
 def Hashing(keyvalue) -> int:
-return keyvalue % len(HashTable)
+	return keyvalue % len(HashTable)
+
 def insert(Hashtable, keyvalue, value) -> None:
-# Write your code here
-Hashtable[Hashing(keyvalue)].append(value)
+	Hashtable[Hashing(keyvalue)].append(value)
+
+
 # Do not edit the following code
 hash_table_size = int(input())
 # Create Hashtable as a list of list.
@@ -18,8 +25,9 @@ HashTable = [[] for _ in range(hash_table_size)]
 input_data = input()
 data = []
 for item in re.split('], |].', input_data):
-item = item[1:]
-data = item.split(', ')
-if len(data) > 1:
-insert(HashTable, int(data[0]), data[1])
+  item = item[1:]
+  data = item.split(', ')
+  if len(data) > 1:
+    insert(HashTable, int(data[0]), data[1])
+
 display_hash (HashTable)
